b608d6ad84eaacfb3e2a2344cffa925f2acfdf0b
yanerobot <screwyuguys@gmail.com>
2024-10-15 01:34:47 +0300

    Significantly improved performace. Fixed backwards animations. Refactor code.
    Performance: Now only 6 drawcalls are registered on the Idle vs 178 in the orginial version and 63 when everything is on.
    Animations: Now properly reverts animation
    Refactor:
    1) ScaleBounce helper coroutine now accept initialScale to support correct "bounce" effect
    2) Now "dynamic" and "static" stars are created separately. Static stars are merged into a single mesh and then destroyed. Dynamic stars exist as gameobjects and disabled when not in use
    3) Removed "Fade" animation

--- Assets/__Scripts/ConstellationDisplay.cs
+++ Assets/__Scripts/ConstellationDisplay.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,68 +7,63 @@ using System;
 
 public class ConstellationDisplay : MonoBehaviour
 {
-	[SerializeField, Tooltip("Lines will stretch instead of fading")] private bool stretchAnimationMode;
 	[SerializeField] private StarDisplay starPrefab;
 	[SerializeField] private Transform GFX;
 	[SerializeField] private Renderer rend;
 	[SerializeField] private LineController linePrefab;
+	[SerializeField] private MeshCombiner meshCombinerPrefab;
 
 	public int LineRendererCount => lines.Count;
-	public Dictionary<int, LineController> UsedLines;
 
 	private ConstellationData constellationData;
-	private List<StarDisplay> stars;
-	private List<LineController> lines;
-	private Transform starsParent;
 	private StarDisplay centerMostStar;
-	
+
+	private List<LineController> lines;
+	public Dictionary<int, LineController> UsedLines;
+	public SortedDictionary<int, List<LineController>> LineAnimationGroups;
+
+	private Dictionary<int, StarDisplay> dynamicStars;
+	private Transform linesParent;
+	private Transform dynamicStarsParent;
+
 	private bool isImageEnabled;
 	private bool isLinesEnabled;
-	private bool isAnimatingLines;
+	private bool isOpeningLines;
+	private bool isClosingLines;
+	private bool linesToggler;
 	private bool isAnimatingImage;
 
 	private bool breakAsync;
 
-
 	private void Awake()
 	{
-		stars = new List<StarDisplay>();
 		lines = new List<LineController>();
 		UsedLines = new Dictionary<int, LineController>();
-		starsParent = new GameObject().transform;
-		starsParent.SetParent(transform.parent);
+		LineAnimationGroups = new SortedDictionary<int, List<LineController>>();
+		dynamicStars = new Dictionary<int, StarDisplay>();
 	}
 
 	public void Init(ConstellationInstanceInfo instanceInfo)
 	{
 		// Инициализируем созвездие
 		constellationData = JsonUtility.FromJson<JsonData>(instanceInfo.JsonAsset.text).items[0];
-		rend.material = instanceInfo.Material;
+		rend.sharedMaterial = instanceInfo.Material;
 		var cam = Camera.main;
-		SetupTransform(cam.transform);
+		SetupTransform(constellationData, cam.transform);
 		var color = rend.material.color;
 		color.a = 0;
 		rend.material.color = color; // rend - отображает изображение созвездия. По умолчанию оно выключено
+		rend.gameObject.SetActive(false);
 
-		// Создаем звезды
-		foreach (var starData in constellationData.stars)
-		{
-			StarDisplay starDisplay = Instantiate(starPrefab, starsParent.transform);
-			starDisplay.Init(starData, this, cam.transform);
-			stars.Add(starDisplay);
-		}
+		CreateStars(cam.transform);
 
-		// Находим самую центральную звезду
-		centerMostStar = stars.Aggregate((minStar, nextStar) =>
-			(constellationData.WorldPos - nextStar.data.WorldPos).magnitude < (constellationData.WorldPos - minStar.data.WorldPos).magnitude ? nextStar : minStar);
-
-		// Создаем соединяющие линии созвездий
-		foreach (var connection in constellationData.pairs)
-		{
-			CreateLine(stars.First(star => star.data.id == connection.from), 
-						stars.First(star => star.data.id == connection.to));
-		}
+		centerMostStar = dynamicStars.Aggregate((minStar, nextStar) =>
+			(constellationData.WorldPos - nextStar.Value.data.WorldPos).magnitude <
+			(constellationData.WorldPos - minStar.Value.data.WorldPos).magnitude ?
+			nextStar : minStar)
+			.Value;
 	}
+
 	public void UpdateLinesWidth()
 	{
 		// Метод для редактирования толщины линии в рантайме
@@ -85,35 +81,95 @@ public class ConstellationDisplay : MonoBehaviour
 		if (isAnimatingImage)
 			return;
 		isAnimatingImage = true;
-		
+		if (isImageEnabled == false)
+			rend.gameObject.SetActive(true);
+
+
 		StartCoroutine(Helper.FadeAnimation(rend, ConstellationManager.Instance.AnimationDuration, !isImageEnabled, OnComplete: () => 
-		{ 
+		{
 			isImageEnabled = !isImageEnabled;
+			if (isImageEnabled == false)
+				rend.gameObject.SetActive(false);
 			isAnimatingImage = false;
 			OnComplete?.Invoke();
 		}));
 	}
 
-	public void AnimateLines(Action OnComplete = null)
-	{
-		// Анимация линий созвездия
-		if (isAnimatingLines)
-			return;
-		isAnimatingLines = true;
 
-		ActivateLinesAnimationAsync(OnComplete);
+	public void ToggleAnimations()
+	{
+		if (linesToggler)
+		{
+			AnimateLinesClose();
+		}
+		else
+		{
+			AnimateLinesOpen();
+		}
 	}
 
-	public void RevertAnimation()
+	public void AnimateLinesOpen(Action OnComplete = null) => AnimateLinesOpenAsync(OnComplete);
+	public void AnimateLinesClose(Action OnComplete = null) => StartCoroutine(AnimateLinesCloseCoroutine(OnComplete));
+
+	#region Private Methods
+
+	private async void AnimateLinesOpenAsync(Action OnComplete = null)
+	{       
+		// Анимация линий созвездия
+		if (isOpeningLines || isLinesEnabled || isClosingLines)
+			return;
+
+		dynamicStarsParent.gameObject.SetActive(true);
+		linesParent.gameObject.SetActive(true);
+		isOpeningLines = true;
+		linesToggler = true;
+
+		// Асинхронный метод для последовательной анимации (без рекурсии)
+		int animationGroup = 0;
+		List<StarDisplay> animatingBatch = await centerMostStar.AnimateAllNeighboursAsync(animationGroup); // Начинаем с самой центральной звезды
+		List<Task<List<StarDisplay>>> tasks = new List<Task<List<StarDisplay>>>(); // Список всех тасков для синхронной анимации
+
+		while (true)
+		{
+			if (breakAsync)
+				break;
+
+			animationGroup++;
+			foreach (var star in animatingBatch)
+			{
+				var item = star.AnimateAllNeighboursAsync(animationGroup);
+				tasks.Add(item);
+			}
+
+			// Если тасков не получено, значит анимации закончились
+			if (tasks.Count == 0)
+				break;
+
+			await Task.WhenAll(tasks);
+
+			animatingBatch.Clear(); // Удаляем уже анимированные звезды
+			foreach (var task in tasks)
+				animatingBatch.AddRange(task.Result); // Добавляем новые (те, что соединены с теми, которые были удалены)
+
+			tasks.Clear();
+		}
+
+		isOpeningLines = false;
+		isLinesEnabled = true;
+		OnComplete?.Invoke();
+	}
+
+	private IEnumerator AnimateLinesCloseCoroutine(Action OnComplete = null)
 	{
 		// Разворот анимации
-		if (UsedLines.Count == 0)
-			return;
-		isAnimatingLines = true;
+		if (LineAnimationGroups.Count == 0 || UsedLines.Count == 0 || isClosingLines)
+			yield break;
+
+		isClosingLines = true;
 
 		// Останавливаем текущие анимации
 		breakAsync = true;
-		foreach (var star in stars)
+		foreach (var star in dynamicStars.Values)
 		{
 			star.StopAllCoroutines();
 			star.ResetScale();
@@ -123,78 +179,36 @@ public class ConstellationDisplay : MonoBehaviour
 
 		// Создаем обратные анимации на тех объектах, которые успели закончить свою анимацию
 		var animDuration = ConstellationManager.Instance.AnimationDuration;
-		foreach (var line in UsedLines.Values)
+		List<LineController> group;
+		for (var i = LineAnimationGroups.Count - 1; i >= 0; i--)
 		{
-			if (stretchAnimationMode)
+			group = LineAnimationGroups[i];
+			foreach (var line in group)
 			{
 				line.RevertTarget();
-				line.StretchLine(animDuration, () =>
-				{
-					OnRevertComplete(line);
-				});
+				line.StretchLine(animDuration, () => line.IsEnabled = false);
 			}
-			else
-			{
-				line.StartCoroutine(Helper.FadeAnimation(line.lineRenderer, animDuration, false, OnComplete: () =>
-				{
-					OnRevertComplete(line);
-				}));
-			}
+
+			yield return new WaitForSeconds(animDuration);
 		}
 
-		void OnRevertComplete(LineController line)
-		{
-			// OnComplete
-			line.IsEnabled = false;
-			isAnimatingLines = false;
-			isLinesEnabled = false;
-			breakAsync = false;
-			UsedLines.Clear();
-		}
-	}
-
-	private async void ActivateLinesAnimationAsync(Action OnComplete = null)
-	{
-		if (isLinesEnabled)
-			return;
-
-		// Асинхронный метод для последовательной анимации (без рекурсии)
-		List<StarDisplay> animatingBatch = await centerMostStar.AnimateAllNeighboursAsync(stretchAnimationMode); // Начинаем с самой центральной звезды
-		List<Task<List<StarDisplay>>> tasks = new List<Task<List<StarDisplay>>>(); // Список всех тасков для синхронной анимации
-
-		while (true)
-		{
-			if (breakAsync)
-				break;
-
-			foreach (var star in animatingBatch)
-			{
-				var item = star.AnimateAllNeighboursAsync(stretchAnimationMode);
-				tasks.Add(item);
-			}
-			
-			// Если тасков не получено, значит анимации закончились
-			if (tasks.Count == 0)
-				break;
-
-			await Task.WhenAll(tasks);
-
-			animatingBatch.Clear(); // Удаляем уже анимированные звезды
-			foreach (var task in tasks) 
-				animatingBatch.AddRange(task.Result); // Добавляем новые (те, что соединены с теми, которые были удалены)
-
-			tasks.Clear();
-		}
-
-		isAnimatingLines = false;
-		isLinesEnabled = true;
+		isOpeningLines = false;
+		isLinesEnabled = false;
+		isClosingLines = false;
+		breakAsync = false;
+		linesToggler = false;
+		LineAnimationGroups.Clear();
+		UsedLines.Clear();
 		OnComplete?.Invoke();
+		linesParent.gameObject.SetActive(false);
+		dynamicStarsParent.gameObject.SetActive(false);
 	}
 
-	private void SetupTransform(Transform camTransform)
+	private void SetupTransform(ConstellationData constellationData, Transform camTransform)
 	{
 		// Настраиваем трансформ для корректного расположения созвездия на небесной сфере
 		transform.position = constellationData.WorldPos;
+		GFX.name = constellationData.name + "_Image";
 		GFX.LookAt(camTransform);
 		camTransform.LookAt(GFX);
 		GFX.transform.position = constellationData.ImagePos;
@@ -206,9 +220,8 @@ public class ConstellationDisplay : MonoBehaviour
 	private void CreateLine(StarDisplay fromStarDisplay, StarDisplay toStarDisplay)
 	{
 		// Создаем нужное кол-во линий (по-умолчанию выключены)
-
-		var line = Instantiate(linePrefab, transform);
-		line.Init(stretchAnimationMode);
+		var line = Instantiate(linePrefab, linesParent);
+		line.Init();
 
 		line.CalcPositions(fromStarDisplay, toStarDisplay);
 		line.SetPosition();
@@ -217,4 +230,63 @@ public class ConstellationDisplay : MonoBehaviour
 		toStarDisplay.AddConnection(line, fromStarDisplay);
 		lines.Add(line);
 	}
+
+	private void CreateStars(Transform lookAt)
+	{
+		CreateDynamicStars(lookAt);
+		CreateStaticStars(lookAt);
+	}
+
+	private void CreateDynamicStars(Transform lookAt)
+	{
+		// Создаем "динамические" звезды (которые участвуют в анимациях = части созвездий)
+		dynamicStarsParent = new GameObject().transform;
+		dynamicStarsParent.SetParent(transform);
+		dynamicStarsParent.name = constellationData.name + "-Stars-Dynamic";
+		dynamicStarsParent.gameObject.SetActive(false);
+
+		linesParent = new GameObject("Lines").transform;
+		linesParent.SetParent(transform); 
+		linesParent.gameObject.SetActive(false);
+
+
+		foreach (var connection in constellationData.pairs)
+		{
+			var fromStarData = constellationData.stars.First(star => star.id == connection.from);
+			var toStarData = constellationData.stars.First(star => star.id == connection.to);
+
+			if (!dynamicStars.ContainsKey(fromStarData.id))
+			{
+				StarDisplay starDisplay = Instantiate(starPrefab, dynamicStarsParent.transform);
+				starDisplay.Init(fromStarData, this, lookAt);
+				dynamicStars.Add(fromStarData.id, starDisplay);
+			}
+
+			if (!dynamicStars.ContainsKey(toStarData.id))
+			{
+				StarDisplay starDisplay = Instantiate(starPrefab, dynamicStarsParent.transform);
+				starDisplay.Init(toStarData, this, lookAt);
+				dynamicStars.Add(toStarData.id, starDisplay);
+			}
+
+			// Создаем соединяющие линии созвездий
+			CreateLine(dynamicStars[fromStarData.id], dynamicStars[toStarData.id]);
+		}
+	}
+
+	private void CreateStaticStars(Transform lookAt)
+	{
+		// Создаем и объединяем статические звезды
+		var meshCombiner = Instantiate(meshCombinerPrefab, transform);
+		meshCombiner.name = constellationData.name + "-Stars-Static";
+		foreach (var starData in constellationData.stars)
+		{
+			StarDisplay starDisplay = Instantiate(starPrefab, meshCombiner.transform);
+			starDisplay.Init(starData, this, lookAt);
+			meshCombiner.Add(starDisplay.MeshSchemaIndex, starDisplay.meshFilter);
+		}
+		meshCombiner.CombineAllMeshes();
+	}
+
+	#endregion
 }
